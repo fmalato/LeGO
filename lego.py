29414718977e0eb3d454a606abcf5c69b6dfd6a4
freaky1310@gmail.com <arthas1310>
2020-03-21 15:47:02 +0100

    check svm

--- lego.py
+++ lego.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from scipy.optimize import minimize
 
 from multistart import generate
@@ -16,7 +17,7 @@ def lego(f, threshold, clf, n_dimensions=2, maxRange=5.12, numSamples=100, numTr
 
     for i in range(numTrainingSamples):
         sample = generate(n_dimensions, maxRange)
-        res = minimize(f, sample, method='nelder-mead', options={'xatol': 1e-8})
+        res = minimize(f, sample, method='L-BFGS-B', options={'ftol': 1e-8})
 
         trainSetX = np.append(trainSetX, sample)
 
@@ -29,44 +30,62 @@ def lego(f, threshold, clf, n_dimensions=2, maxRange=5.12, numSamples=100, numTr
             actualBest = res['fun']
             bestPoint = res.x
 
-    # stats
-    positives = 0
-    for i in range(len(trainSetY)):
-        if trainSetY[i] == np.array(1):
-            positives += 1
-    print('Positive examples: ' + str(positives) + '/1000')
-
     trainSetX = trainSetX.reshape(numTrainingSamples, n_dimensions)
     trainSetY = trainSetY.reshape(-1, 1)
 
-    clf.fit(trainSetX, trainSetY)
+    xTrain, xTest, yTrain, yTest = train_test_split(trainSetX, trainSetY, test_size=0.25)
 
-    for i in range(numSamples):
+    # stats
+    positives = 0
+    for i in range(len(yTrain)):
+        if yTrain[i] == np.array(1):
+            positives += 1
+    print('Positive examples: ' + str(positives) + '/' + str(len(yTrain)))
+
+    # training
+    clf.fit(xTrain, yTrain)
+    # validating
+    predictions = clf.predict(xTest)
+    right = 0
+    for i in range(len(predictions)):
+        if predictions[i] == yTest[i]:
+            right += 1
+    print('Accuracy: ' + str((right / len(predictions)) * 100) + '%')
+
+    stats = []
+    numRuns = 0
+    print('Progress: 0/' + str(numSamples))
+    while len(stats) < numSamples:
         sample = generate(n_dimensions, maxRange)
-        if clf.predict([sample]) is 1:
-            res = minimize(f, sample, method='nelder-mead', options={'xatol': 1e-8})
+        if clf.predict([sample])[0] == 1.0:
+            res = minimize(f, sample, method='L-BFGS-B', options={'ftol': 1e-8})
             if res['fun'] < actualBest:
                 actualBest = res['fun']
                 bestPoint = res.x
+            stats.append(res['fun'])
+            if len(stats) % 20 == 0:
+                print('Progress: ' + str(len(stats)) + '/' + str(numSamples))
+        numRuns += 1
 
-    return actualBest, bestPoint
+    goodOptChance = 0
+    for i in range(len(stats)):
+        if stats[i] < threshold:
+            goodOptChance += 1
+
+    return actualBest, bestPoint, goodOptChance, numRuns
 
 
 clf = SVC(gamma='auto')
 results = []
-for i in range(300):
-    print('Iteration ' + str(i))
-    start = time.time()
-    best, point = lego(rastrigin, threshold=2, clf=clf, n_dimensions=2, maxRange=5.12)
-    results.append(best)
-    end = time.time()
-    print('best: ' + str(best) + '    point: ' + str(point) + '    time elapsed: ' + str(end - start))
+numSamples = 100
+threshold = 1
 
-globalOptChance = 0
-for i in range(len(results)):
-    if results[i] == 0.0:
-        globalOptChance += 1
-
-print('Global optimum was found ' + str(globalOptChance) + ' times out of 300 trials.')
+start = time.time()
+best, point, goodOptChance, numRuns = lego(rastrigin, threshold=threshold, clf=clf, numSamples=numSamples,
+                                           n_dimensions=2, maxRange=5.12)
+results.append(best)
+end = time.time()
+print('best: ' + str(best) + '    point: ' + str(point) + '    time elapsed: ' + str(end - start))
+print('An acceptable optimum was found ' + str(goodOptChance) + ' times out of ' + str(numRuns) + ' trials.')
 
 
